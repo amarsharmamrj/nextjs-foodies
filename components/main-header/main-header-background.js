8271641f18d9cef7c9d6bf37506377e567b08e42
Amarnath Vishwakarma <amarsharmamrj@gmail.com>
2025-03-08 10:31:23 +0530

    normal changes

--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -7,7 +7,7 @@ export default function MainHeaderBackground() {
         <defs>
           <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
             <stop
-              offset="0%"
+              offset="0%" 
               style={{ stopColor: '#59453c', stopOpacity: '1' }}
             />
             <stop
