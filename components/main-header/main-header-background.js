68519a4136404674b977dc73097f0bcbeada2861
Amarnath Vishwakarma <amarsharmamrj@gmail.com>
2025-03-15 10:49:15 +0530

    changes

--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -4,7 +4,7 @@ export default function MainHeaderBackground() {
   return (
     <div className={classes['header-background']}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <defs>
+        <defs> 
           <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
             <stop
               offset="0%" 
