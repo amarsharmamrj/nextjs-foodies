5d04b1ed6901152d91af48b80fd18f05a337be0e
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2025-04-14 10:01:40 +0530

    Update main-header-background.js

--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -1,5 +1,5 @@
 import classes from './main-header-background.module.css';    
-  
+   
 export default function MainHeaderBackground() {
   return (
     <div className={classes['header-background']}> 
